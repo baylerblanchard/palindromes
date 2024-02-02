e93118f1cbd1a8cf7839829580f307449ff80288
baylerblanchard <baylerblanchard@gmail.com>
2024-02-02 11:43:42 -0600

    finished the palindrome example

--- palindromes.js
+++ palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reverseString = string.toLowerCase().split('').reverse().join('').replace(/[^a-zA-Z0-9]/g, '');
+    let originalString = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    return reverseString === originalString;
 };
 
 // Do not edit below this line
